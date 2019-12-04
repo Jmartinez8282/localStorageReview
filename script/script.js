24de283e7281bc1452a1b89ea3df9c4483524cae
Code <code@COCTL-41256.local>
2019-12-04 11:20:10 -0800

    cp

--- script/script.js
+++ script/script.js
@@ -1,14 +1,39 @@
+let exampleData;
+if (localStorage.getItem('localArrayKey')) {
+    console.log('local storage exist and has been loaded');
+    exampleData = JSON.parse(localStorage.getItem('localArrayKey'))
+} else {
+    exampleData = [];
+}
 
-let exampleData = [ 'Lincoln','Daniel','JT'];
+//let exampleData = ['Lincoln', 'Daniel', 'JT'];
 
 let setLocalBtn = document.getElementById('setLocalBtn');
 let consolelogLocalBtn = document.getElementById('consolelogLocalBtn');
+let inputArea = document.getElementById('inputArea');
+let sumitBtn = document.getElementById('sumitBtn');
+let dropdownMenuButton = document.getElementById('dropdownMenuButton');
+let elementArea = document.getElementById('elementAre');
 
+consolelogLocalBtn.addEventListener('click', function (e) {
 
-setLocalBtn.addEventListener('click',function(e){
-localStorage.setItem('localArrayKey',exampleData);
+    let testArray = JSON.parse(localStorage.getItem('localArrayKey'));
+    console.log(testArray);
 });
+sumitBtn.addEventListener('click',function(e){
 
-consolelogLocalBtn.addEventListener('click',function(e){
-console.log(localStorage.getItem('localArrayKey'));
+let obj = {
+title: inputArea.value,
+listItems: []
+} 
+exampleData.push(obj);
+saveData();
+inputArea.value="";
 });
+function saveData(){
+    localStorage.setItem('localArrayKey',JSON.stringify(exampleData));
+}
+
+function anakin(chamber){
+
+}
